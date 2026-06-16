534b7b1ead1f18f0508fd607da2721c09e8f6d6b
Bibin V Santhosh <bibinvsanthosh@gmail.com>
2026-06-16 14:46:16 +0530

    refactor: separate business logic into services layer

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from services.app1_service import App1Service
 
 # Create your views here.
+# app1/views.py
+
+
+
+# app1/views.py
+
+
+
+
+def home(request):
+    message = App1Service.get_welcome_message()
+    return HttpResponse(message)
---
+++ services/app1_service.py
@@ -0,0 +1,7 @@
+# services/app1_service.py
+
+class App1Service:
+
+    @staticmethod
+    def get_welcome_message():
+        return "Welcome to Zecpath"
